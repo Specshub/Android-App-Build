532bcd7383904e660fc77ddf8daf82e34ba7fd3f
Fonx <arssas71@gmail.com>
2026-04-04 02:10:13 +0100

    Create Constants.kt

---
+++ app/src/main/java/com/iptv/player/Constants.kt
@@ -0,0 +1,7 @@
+package com.iptv.player
+
+object Constants {
+    const val EXTRA_HOST = "extra_host"
+    const val EXTRA_USERNAME = "extra_username"
+    const val EXTRA_PASSWORD = "extra_password"
+}
