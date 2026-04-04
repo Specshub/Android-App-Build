c217d3efbe69423ae8bf0cc9df652cd430c25ecc
Fonx <arssas71@gmail.com>
2026-04-04 02:38:52 +0100

    Delete app/src/main/java/com/iptv/player/AppConstants.kt

--- app/src/main/java/com/iptv/player/AppConstants.kt
+++
@@ -1,6 +0,0 @@
-package com.iptv.player
-
-// تعريف الثوابت بشكل عام لكي يراها التطبيق بالكامل
-const val EXTRA_HOST = "EXTRA_HOST"
-const val EXTRA_USERNAME = "EXTRA_USERNAME"
-const val EXTRA_PASSWORD = "EXTRA_PASSWORD"
