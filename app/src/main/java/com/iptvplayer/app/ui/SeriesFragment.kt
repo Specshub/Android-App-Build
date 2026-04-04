403a8cd500d2aca46a5b56a951aa4ef48e3b4218
Fonx <arssas71@gmail.com>
2026-04-04 01:47:30 +0100

    Add SeriesFragment for displaying series
    Implement SeriesFragment to display series in a grid layout.

---
+++ app/src/main/java/com/iptvplayer/app/ui/SeriesFragment.kt
@@ -0,0 +1,33 @@
+package com.iptvplayer.app.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import com.iptvplayer.app.databinding.FragmentSeriesBinding
+
+class SeriesFragment : Fragment() {
+    private var _binding: FragmentSeriesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSeriesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // عرض المسلسلات في شبكة من 3 أعمدة
+        binding.recyclerView.layoutManager = GridLayoutManager(context, 3)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
