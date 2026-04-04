9bad578a6b5b36a5e6c544514e025fdd98800c8c
Fonx <arssas71@gmail.com>
2026-04-04 02:32:17 +0100

    Add AppConstants for application-wide constants

---
+++ app/src/main/java/com/iptv/player/AppConstants.kt
@@ -0,0 +1,6 @@
+package com.iptv.player
+
+// تعريف الثوابت بشكل عام لكي يراها التطبيق بالكامل
+const val EXTRA_HOST = "EXTRA_HOST"
+const val EXTRA_USERNAME = "EXTRA_USERNAME"
+const val EXTRA_PASSWORD = "EXTRA_PASSWORD"
